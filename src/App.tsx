5c22ba176cd4dda084d298dd63fa6f611405c4c6
Lorenzo Palumbo <lorenzopalumbo.dev@gmail.com>
2024-09-16 22:01:24 +0200

    init demo

--- src/App.tsx
+++ src/App.tsx
@@ -110,135 +110,142 @@ const App = () => {
 
   return (
     <div className="container">
-      <div>
-        <h1>React-blur-img Component</h1>
-        <h3>Change the settings to watch the differences</h3>
-        <hr />
-        <h2>Image settings</h2>
-        <div className="input-container">
-          <label htmlFor="object-position">
-            Select aspect ratio:
-            <select
-              id="aspect-ratio"
-              name="aspect-ratio"
-              onChange={(e) =>
-                setImgSettings((prev) => ({
-                  ...prev,
-                  aspectRatio: e.target.value,
-                }))
-              }
-            >
-              <option value="1/1">1:1</option>
-              <option value="16/9">16:9</option>
-              <option value="4/3">4:3</option>
-              <option value="3/2">3:2</option>
-              <option value="auto">Auto</option>
-            </select>
-          </label>
-          <label htmlFor="object-position">
-            Select object fit:
-            <select
-              id="object-fit"
-              name="object-fit"
-              onChange={(e) =>
-                setImgSettings((prev) => ({
-                  ...prev,
-                  objectFit: e.target.value,
-                }))
-              }
-            >
-              <option value="cover">Cover</option>
-              <option value="fill">Fill</option>
-              <option value="none">None</option>
-            </select>
-          </label>
-          <label htmlFor="object-position">
-            Select object position:
-            <select
-              id="object-position"
-              name="object-position"
-              onChange={(e) =>
-                setImgSettings((prev) => ({
-                  ...prev,
-                  objectPosition: e.target.value,
-                }))
-              }
-            >
-              <option value="center">Center</option>
-              <option value="top left">Top Left</option>
-              <option value="top center">Top Center</option>
-              <option value="top right">Top Right</option>
-              <option value="middle left">Middle Left</option>
-              <option value="middle center">Middle Center</option>
-              <option value="middle right">Middle Right</option>
-              <option value="bottom left">Bottom Left</option>
-              <option value="bottom center">Bottom Center</option>
-              <option value="bottom right">Bottom Right</option>
-            </select>
-          </label>
+      <div className="settings-container">
+        <div>
+          <h1>React-blur-img Component</h1>
+          <h3>Change the settings to watch the differences</h3>
+          <span>
+            If your internet doesn't allow you to see the animation, open
+            Inspect, go to the Network tab, and set a slower Wi-Fi speed.
+          </span>
         </div>
         <hr />
-        <h2>Spinner settings</h2>
-        <div className="spinner-settings-container">
-          <p>
-            Set only the border or the borderTop; if you set both parameters,
-            the border will override the borderTop
-          </p>
-          <div className="input-container spinner-settings">
-            <Input
-              initialValue={40}
-              type="number"
-              text="width"
-              handleChange={handleChange}
-            />
-            <Input
-              initialValue={40}
-              type="number"
-              text="height"
-              handleChange={handleChange}
-            />
-            <label>
-              Select a Spinner Style
+        <div>
+          <h2>Image settings</h2>
+          <div className="input-container">
+            <label htmlFor="object-position">
+              Select aspect ratio:
               <select
-                id="border-style"
-                name="border-style"
+                id="aspect-ratio"
+                name="aspect-ratio"
                 onChange={(e) =>
-                  setSpinner((prev) => ({ ...prev, style: e.target.value }))
+                  setImgSettings((prev) => ({
+                    ...prev,
+                    aspectRatio: e.target.value,
+                  }))
                 }
               >
-                <option value="none">None</option>
-                <option value="solid">Solid</option>
-                <option value="dotted">Dotted</option>
-                <option value="dashed">Dashed</option>
-                <option value="double">Double</option>
-                <option value="groove">Groove</option>
-                <option value="ridge">Ridge</option>
-                <option value="inset">Inset</option>
-                <option value="outset">Outset</option>
-                <option value="hidden">Hidden</option>
+                <option value="1/1">1:1</option>
+                <option value="16/9">16:9</option>
+                <option value="4/3">4:3</option>
+                <option value="3/2">3:2</option>
+                <option value="auto">Auto</option>
               </select>
             </label>
-            <Input
-              type="text"
-              text="color"
-              handleChange={handleChange}
-              placeHolder="eg. white or rgb(122,122,122)"
-            />
-            <Input
-              type="text"
-              text="border"
-              handleChange={handleChange}
-              placeHolder="eg. 4px solid white"
-            />
-            <Input
-              type="text"
-              text="borderTop"
-              handleChange={handleChange}
-              placeHolder="eg. 4px solid white"
-            />
+            <label htmlFor="object-position">
+              Select object fit:
+              <select
+                id="object-fit"
+                name="object-fit"
+                onChange={(e) =>
+                  setImgSettings((prev) => ({
+                    ...prev,
+                    objectFit: e.target.value,
+                  }))
+                }
+              >
+                <option value="cover">Cover</option>
+                <option value="fill">Fill</option>
+                <option value="none">None</option>
+              </select>
+            </label>
+            <label htmlFor="object-position">
+              Select object position:
+              <select
+                id="object-position"
+                name="object-position"
+                onChange={(e) =>
+                  setImgSettings((prev) => ({
+                    ...prev,
+                    objectPosition: e.target.value,
+                  }))
+                }
+              >
+                <option value="center">Center</option>
+                <option value="top left">Top Left</option>
+                <option value="top center">Top Center</option>
+                <option value="top right">Top Right</option>
+                <option value="middle left">Middle Left</option>
+                <option value="middle center">Middle Center</option>
+                <option value="middle right">Middle Right</option>
+                <option value="bottom left">Bottom Left</option>
+                <option value="bottom center">Bottom Center</option>
+                <option value="bottom right">Bottom Right</option>
+              </select>
+            </label>
           </div>
-          <button onClick={() => reloadImages()}>reload</button>
         </div>
+        <hr />
+        <div className="spinner-settings-container"></div>
+        <h2>Spinner settings</h2>
+        <p>
+          Set only the border or the borderTop; if you set both parameters, the
+          border will override the borderTop
+        </p>
+        <div className="input-container spinner-settings">
+          <Input
+            initialValue={40}
+            type="number"
+            text="width"
+            handleChange={handleChange}
+          />
+          <Input
+            initialValue={40}
+            type="number"
+            text="height"
+            handleChange={handleChange}
+          />
+          <label>
+            Select a Spinner Style
+            <select
+              id="border-style"
+              name="border-style"
+              onChange={(e) =>
+                setSpinner((prev) => ({ ...prev, style: e.target.value }))
+              }
+            >
+              <option value="none">None</option>
+              <option value="solid">Solid</option>
+              <option value="dotted">Dotted</option>
+              <option value="dashed">Dashed</option>
+              <option value="double">Double</option>
+              <option value="groove">Groove</option>
+              <option value="ridge">Ridge</option>
+              <option value="inset">Inset</option>
+              <option value="outset">Outset</option>
+              <option value="hidden">Hidden</option>
+            </select>
+          </label>
+          <Input
+            type="text"
+            text="color"
+            handleChange={handleChange}
+            placeHolder="eg. white or rgb(122,122,122)"
+          />
+          <Input
+            type="text"
+            text="border"
+            handleChange={handleChange}
+            placeHolder="eg. 4px solid white"
+          />
+          <Input
+            type="text"
+            text="borderTop"
+            handleChange={handleChange}
+            placeHolder="eg. 4px solid white"
+          />
+        </div>
+        <button onClick={() => reloadImages()}>reload</button>
       </div>
       <div className="images">
         {!reload &&
